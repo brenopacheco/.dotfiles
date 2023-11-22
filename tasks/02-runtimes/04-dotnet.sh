1d9cb34cb25decb000e87220611775c8875bac1b
Breno Pacheco <brenoleonhardt@gmail.com>
2023-11-22 13:44:01 +0000

    Wed Nov 22 01:44:04 PM WET 2023

--- tasks/02-runtimes/04-dotnet.sh
+++ tasks/02-runtimes/04-dotnet.sh
@@ -1,18 +1,11 @@
 #!/usr/bin/env bash
 
-# TODO: fix this. something is not right, where does /bin/dotnet come from?
 function should_run() {
-	return "$SKIP"
 	which dotnet && return "$DONE" || return "$RUN"
 }
 
 function task() {
-	asdf plugin-add dotnet-core https://github.com/emersonsoares/asdf-dotnet-core.git
-	asdf install dotnet-core 5 &&
-		asdf install dotnet-core 6 &&
-		asdf install dotnet-core 7 &&
-		asdf global dotnet-core 7 &&
-		asdf reshim &&
+	sudo pacman -S --noconfirm dotnet-runtime &&
 		curl -sSL https://dot.net/v1/dotnet-install.sh -o /tmp/dotnet-install.sh &&
 		chmod +x /tmp/dotnet-install.sh &&
 		sudo /tmp/dotnet-install.sh --install-dir /usr/share/dotnet -channel STS -version latest &&
