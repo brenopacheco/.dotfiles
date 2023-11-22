15e7a9a93406cbdb36f04725427481f864722511
Breno Pacheco <brenoleonhardt@gmail.com>
2023-11-22 12:45:08 +0000

    Wed Nov 22 12:45:09 PM WET 2023

--- tasks/03-tools/02-tasker.sh
+++ tasks/03-tools/02-tasker.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 function should_run() {
-	has_packages tasker-go && return "$DONE" || return "$RUN"
+	has_packages tasker-go && return "$DONE"
 	gpg --list-secret-keys brenoleonhardt@gmail.com || return "$SKIP"
 	gpg -d "$HOME/.netrc.gpg" | grep "brenoleonhardt@gmail.com" || return "$SKIP"
+	return "$RUN"
 }
 
 function task() {
