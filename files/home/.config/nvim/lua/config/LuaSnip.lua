80373c3f41461d2b067865a9adfea9d83edf0155
Breno Pacheco <brenoleonhardt@gmail.com>
2023-12-26 23:43:00 +0000

    Tue 26 Dec 2023 11:43:01 PM WET

--- files/home/.config/nvim/lua/config/LuaSnip.lua
+++ files/home/.config/nvim/lua/config/LuaSnip.lua
@@ -3,19 +3,4 @@ require('luasnip').setup({
 	delete_check_events = 'TextChanged',
 })
 
-
-require("luasnip.loaders.from_vscode").load()
-
--- require("luasnip.loaders.from_vscode").load({paths = "./my_snippets"})
-
--- keys = {
---     {
---       "<tab>",
---       function()
---         return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
---       end,
---       expr = true, silent = true, mode = "i",
---     },
---     { "<tab>", function() require("luasnip").jump(1) end, mode = "s" },
---     { "<s-tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
---   },
+require('luasnip.loaders.from_snipmate').load({ paths = { vim.fn.stdpath('config') .. "/snippets" } })
