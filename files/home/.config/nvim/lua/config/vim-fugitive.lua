4a2911422aa34b759cb9c3e0bdb878b8ede79ebd
Breno Pacheco <brenoleonhardt@gmail.com>
2024-01-14 23:35:27 +0000

    Sun 14 Jan 2024 11:35:35 PM WET

--- files/home/.config/nvim/lua/config/vim-fugitive.lua
+++ files/home/.config/nvim/lua/config/vim-fugitive.lua
@@ -1,1 +1,10 @@
 vim.g.fugitive_summary_format = '%cs || %<(20,trunc)%an || %s'
+
+vim.api.nvim_create_autocmd({ 'Filetype' }, {
+	desc = 'Goto top of gitcommit buffer',
+	pattern = 'gitcommit',
+	group = vim.api.nvim_create_augroup('fugitive-config', { clear = true }),
+	callback = function()
+		vim.cmd('normal! gg')
+	end,
+})
