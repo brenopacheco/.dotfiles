c04b7fc3d892d25790329b7ecbf31ad1b00f4171
Breno Leonhardt Pacheco <brenoleonhardt@gmail.com>
2024-11-25 01:45:28 +0000

    Mon 25 Nov 2024 01:45:28 AM WET

--- playbook.pl
+++ playbook.pl
@@ -7,38 +7,34 @@ configure (
     user => $ENV{'USER'}
 );
 
-# ensure(
-#     [ 'misc/known_hosts', '~/.ssh/known_hosts'    ],
-#     [ 'misc/crontab',     '/var/spool/cron/breno' ]
-# );
-
-clone(
-    [ 'git@github.com:brenopacheco/.dotfiles.git',       '~/.dotfiles'         ],
-    [ 'git@github.com:brenopacheco/.password-store.git', '~/.password-store'   ],
-    [ 'git@github.com:brenopacheco/notes.git',           '~/notes'             ],
-    [ 'git@github.com:brenopacheco/dwm-fork.git',        '~/git/dwm-fork'      ],
-    [ 'git@github.com:brenopacheco/st-fork.git',         '~/git/st-fork'       ],
-    [ 'git@github.com:brenopacheco/dmenu-fork.git',      '~/git/dmenu-fork'    ],
-    [ 'git@github.com:brenopacheco/slstatus-fork.git',   '~/git/slstatus-fork' ]
+ensure(
+    [ 'misc/known_hosts', '~/.ssh/known_hosts'    ],
+    [ 'misc/crontab',     '/var/spool/cron/breno' ]
 );
 
-# clone(
-#     ['https://github.com/asdf-vm/asdf.git --branch v0.14.1', '~/.asdf' ]
-# );
+clone(
+    [ 'git@github.com:brenopacheco/.dotfiles.git',            '~/.dotfiles'         ],
+    [ 'git@github.com:brenopacheco/.password-store.git',      '~/.password-store'   ],
+    [ 'git@github.com:brenopacheco/notes.git',                '~/notes'             ],
+    [ 'git@github.com:brenopacheco/dwm-fork.git',             '~/git/dwm-fork'      ],
+    [ 'git@github.com:brenopacheco/st-fork.git',              '~/git/st-fork'       ],
+    [ 'git@github.com:brenopacheco/dmenu-fork.git',           '~/git/dmenu-fork'    ],
+    [ 'git@github.com:brenopacheco/slstatus-fork.git',        '~/git/slstatus-fork' ],
+    [ 'https://github.com/asdf-vm/asdf.git --branch v0.14.1', '~/.asdf'             ]
+);
 
-# asdf(
-#     plugin => "nodejs",
-#     url => "https://github.com/twuni/asdf-yarn.git",
-#     versions => [ "23.3.0", "22.11.0", "21.7.3", "20.18.1", "19.9.0", "18.20.5" ],
-#     global => "23.3.0"
-# );
-
-# asdf(
-#     plugin => "yarn",
-#     url => "https://github.com/twuni/asdf-yarn.git",
-#     versions => [ "1.20.0" ],
-#     global => "1.20.0"
-# );
+asdf(
+    nodejs => {
+        url => "https://github.com/twuni/asdf-yarn.git",
+        versions => [ "23.3.0", "22.11.0", "21.7.3", "20.18.1", "19.9.0", "18.20.5" ],
+        global => "23.3.0"
+    },
+    yarn => {
+        url => "https://github.com/twuni/asdf-yarn.git",
+        versions => [ "1.20.0" ],
+        global => "1.20.0"
+    }
+);
 
 pacman(
     'arandr', 'base', 'base-devel', 'bash-completion', 'bat', 'bear',
@@ -61,73 +57,69 @@ pacman(
     'xorg-xwininfo', 'zathura', 'zathura-pdf-mupdf', 'zip', 'zk'
 );
 
-# stow(cwd => '~/.dotfiles', target => '~/', package => 'home');
+stow(cwd => '~/.dotfiles', target => '~/', package => 'home');
 
-# dirs('git', 'sketch', 'tmp');
+dirs('git', 'sketch', 'tmp');
 
-# etc(
-#     { file => 'etc/lightdm-gtk-greeter.conf', dir => '/etc/lightdm/'         },
-#     { file => 'etc/10-keyboard.rules',        dir => '/etc/udev/rules.d/'    },
-#     { file => 'etc/40-wacom.rules',           dir => '/etc/udev/rules.d/'    },
-#     { file => 'etc/10-keyboard.conf',         dir => '/etc/X11/xorg.conf.d/' },
-#     { file => 'etc/20-monitor.conf',          dir => '/etc/X11/xorg.conf.d/' },
-#     { file => 'etc/30-touchpad.conf',         dir => '/etc/X11/xorg.conf.d/' },
-# );
+etc(
+    { file => 'etc/lightdm-gtk-greeter.conf', dir => '/etc/lightdm/'         },
+    { file => 'etc/10-keyboard.rules',        dir => '/etc/udev/rules.d/'    },
+    { file => 'etc/40-wacom.rules',           dir => '/etc/udev/rules.d/'    },
+    { file => 'etc/10-keyboard.conf',         dir => '/etc/X11/xorg.conf.d/' },
+    { file => 'etc/20-monitor.conf',          dir => '/etc/X11/xorg.conf.d/' },
+    { file => 'etc/30-touchpad.conf',         dir => '/etc/X11/xorg.conf.d/' },
+);
 
-# systemctl(
-#     who => 'root',
-#     svc => [ 'bluetooth', 'cronie', 'docker', 'lightdm', 'sshd' ]
-# );
+systemctl(
+    root => [ 'bluetooth', 'cronie', 'docker', 'lightdm', 'sshd' ],
+    user => [ 'gpg-agent.target' ]
+);
 
-# systemctl(
-#     who => 'user',
-#     svc => [ 'gpg-agent.target' ]
-# );
+makepkg(
+    'makepkg/fork' => [
+        'dmenu-fork',
+        'dwm-fork',
+        'slstatus-fork',
+        'st-fork',
+        'neovim'
+    ],
+    'makepkg/aur'  => [
+        'bun',
+        'helm-ls',
+        'lls-addons',
+        'marksman',
+        'obs-backgroundremoval',
+        'rose-pine-gtk-theme-full',
+        'slack-desktop',
+        'vscode-js-debug',
+        'xcursor-breeze',
+        'zoom'
+    ]
+);
 
-# makepkg(
-#     dir => 'makepkg/fork',
-#     pkg => ['dmenu-fork', 'dwm-fork', 'slstatus-fork', 'st-fork', 'neovim']
-# );
+node(
+    [ 'bash-language-server',              'bash-language-server'              ],
+    [ 'eslint',                            'eslint'                            ],
+    [ 'prettier',                          'prettier'                          ],
+    [ 'prettierd',                         '@fsouza/prettierd'                 ],
+    [ 'tsc',                               'typescript'                        ],
+    [ 'tsserver',                          'typescript-language-server'        ],
+    [ 'vim-language-server',               'vim-language-server'               ],
+    [ 'yaml-language-server',              'yaml-language-server'              ],
+    [ 'vscode-eslint-language-server',     'vscode-langservers-extracted'      ],
+    [ 'docker-langserver',                 'dockerfile-language-server-nodejs' ]
+);
 
-# makepkg(
-#     dir => 'makepkg/aur',
-#     pkg => [
-#         'bun',
-#         'helm-ls',
-#         'lls-addons',
-#         'marksman',
-#         'obs-backgroundremoval',
-#         'rose-pine-gtk-theme-full',
-#         'slack-desktop',
-#         'vscode-js-debug',
-#         'xcursor-breeze',
-#         'zoom'
-#     ]
-# );
-
-# node(
-#     [ 'bash-language-server',              'bash-language-server'              ],
-#     [ 'eslint',                            'eslint'                            ],
-#     [ 'prettier',                          'prettier'                          ],
-#     [ 'prettierd',                         '@fsouza/prettierd'                 ],
-#     [ 'tsc',                               'typescript'                        ],
-#     [ 'tsserver',                          'typescript-language-server'        ],
-#     [ 'vim-language-server',               'vim-language-server'               ],
-#     [ 'yaml-language-server',              'yaml-language-server'              ],
-#     [ 'vscode-eslint-language-server',     'vscode-langservers-extracted'      ],
-#     [ 'docker-langserver',                 'dockerfile-language-server-nodejs' ]
-# );
-
-# go(
-#     [ 'efmls',      'github.com/mattn/efm-langserver@latest'     ],
-#     [ 'godoc',      'golang.org/x/tools/cmd/godoc@latest'        ],
-#     [ 'gopls',      'golang.org/x/tools/gopls@latest'            ],
-#     [ 'kind',       'sigs.k8s.io/kind@v0.20.0'                   ],
-#     [ 'lazydocker', 'github.com/jesseduffield/lazydocker@latest' ],
-#     [ 'lf',         'github.com/gokcehan/lf@latest'              ],
-#     [ 'shfmt',      'mvdan.cc/sh/v3/cmd/shfmt@latest'            ],
-#     [ 'yq',         'github.com/mikefarah/yq/v4@latest'          ]
-# );
+go(
+    [ 'efmls',      'github.com/mattn/efm-langserver@latest'     ],
+    [ 'godoc',      'golang.org/x/tools/cmd/godoc@latest'        ],
+    [ 'gopls',      'golang.org/x/tools/gopls@latest'            ],
+    [ 'kind',       'sigs.k8s.io/kind@v0.20.0'                   ],
+    [ 'lazydocker', 'github.com/jesseduffield/lazydocker@latest' ],
+    [ 'lf',         'github.com/gokcehan/lf@latest'              ],
+    [ 'shfmt',      'mvdan.cc/sh/v3/cmd/shfmt@latest'            ],
+    [ 'yq',         'github.com/mikefarah/yq/v4@latest'          ]
+);
 
 # }}} ------------------------------------------------------------------------
 # Library ================================================================ {{{
@@ -135,21 +127,24 @@ use diagnostics;
 use feature 'say';
 
 sub _system {
-    my ($cmd) = @_;
+    my ($shout, $cmd) = @_;
     my @out = `$cmd 2>&1`;
-    print "  | $_" for (@out);
+    if ($shout) {
+        print "  | $_" for (@out);
+    }
     if ($? != 0) {
         print STDERR "\n[ERROR] command failed: $cmd\n";
         exit 1;
     }
+    return @out;
 }
 
 sub configure {
     my (%cfg) = @_;
     say "[OK] configuring";
-    say "  * host: $cfg{host}";
-    say "  * user: $cfg{user}";
-    say "  * root: $cfg{root}";
+    # say "  * host: $cfg{host}";
+    # say "  * user: $cfg{user}";
+    # say "  * root: $cfg{root}";
 }
 
 # TODO: what do we want to ensure?
@@ -157,7 +152,7 @@ sub configure {
 # 2. lines are in included in the file
 # 3. respect original file ownership and permissions
 sub ensure {
-    say "ensure";
+    say "[TODO] ensure";
 }
 
 sub clone {
@@ -170,20 +165,24 @@ sub clone {
     for (@res) {
         my ($repo, $dir) = (@{$_});
         printf "  * cloning: %-${mlen}s -> %s\n", $repo, $dir;
-        _system("git clone $repo $dir");
+        _system(1, "git clone $repo $dir");
     }
 }
 
-# stow -n --adopt -v 2 -t $HOME/ home 2>&1
 sub stow {
     my (%cfg) = @_;
-    my $res = `stow --adopt -v 2 -d $cfg{cwd} -t $cfg{target} $cfg{package} 2>&1`;
-    die "[ERROR] stow:\n$res" if $? != 0;
-    say "> [OK] stow\n";
+    my $args = "--adopt -v 2 -d $cfg{cwd} -t $cfg{target} $cfg{package}";
+    my @links = grep { /^LINK/g } _system(0, "stow -n $args 2>&1");
+    if (!@links) {
+        return say "[OK] stow";
+    }
+    say "[-] stow";
+    say "  * stowing: $cfg{cwd}/$cfg{package} => $cfg{target}";
+    _system(1, "stow $args 2>&1 | grep 'LINK'");
 }
 
 sub asdf {
-    say "asdf";
+    say "[TODO] asdf";
 }
 
 sub pacman {
@@ -195,21 +194,16 @@ sub pacman {
     say "[-] pacman";
     for my $package (@packages) {
         say "  * installing: $package" ;
-        _system("sudo pacman -S --noconfirm --needed $package");
-        # system("sudo pacman -S --noconfirm --needed $package") == 0 or die $res;
+        _system(1, "sudo pacman -S --noconfirm --needed $package");
     }
 }
 
 sub dirs {
-    say "mkdir";
+    say "[TODO] dirs";
 }
 
 sub systemctl {
-    say "systemctl";
-    # my ($who, $services) = @_;
-    # for (@{$services}) {
-    #     say "Configuring service $_ ($who)";
-    # }
+    say "[TODO] systemctl";
 }
 
 # etc will scp all files in sync to a temporary dir, change permissions to
@@ -217,24 +211,19 @@ sub systemctl {
 # copying the file if they are not. files will be owned by root and
 # permissions are kept
 sub etc {
-    say "etc";
-    # for (@_) {
-    #     my ($file, $target) = @{$_};
-    #     say "Syncing $file to $target";
-    # }
+    say "[TODO] etc";
 }
 
 sub makepkg {
-    say "makepkg";
-    # say "Installing (makepkg) @_";
+    say "[TODO] makepkg";
 }
 
 sub node {
-    say "node";
+    say "[TODO] node";
 }
 
 sub go {
-    say "go";
+    say "[TODO] go";
 }
 
 # }}} ------------------------------------------------------------------------
