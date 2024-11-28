c7c16dc239a8aa00592dcc4d97d912641c6a225c
Breno Leonhardt Pacheco <brenoleonhardt@gmail.com>
2024-11-28 17:47:00 +0000

    Thu 28 Nov 2024 05:47:01 PM WET

--- playbook.pl
+++ playbook.pl
@@ -1,16 +1,12 @@
 #!/usr/bin/env perl
 
 # TODO:
-# 1. swap `` calls for _system or something else
-# 2. complete subs
-#   - asdf
-#   - node
-#   - go
-#   - makepkg
-#   - etc
-#   - systemctl
-# 3. complete ensure sub
-# 4. make _system use ssh connection
+# 1. [ ] complete subs
+#   - [ ] systemctl
+#   - [ ] makepkg
+#   - [ ] etc
+# 2. [ ] complete ensure sub
+# 3. [ ] make _system use ssh connection
 
 # Playbook =============================================================== {{{
 configure (
@@ -19,10 +15,10 @@ configure (
     user => $ENV{'USER'}
 );
 
-ensure(
-    [ 'misc/known_hosts', '~/.ssh/known_hosts'    ],
-    [ 'misc/crontab',     '/var/spool/cron/breno' ]
-);
+# ensure(
+#     [ 'misc/known_hosts', '~/.ssh/known_hosts'    ],
+#     [ 'misc/crontab',     '/var/spool/cron/breno' ]
+# );
 
 clone(
     [ 'git@github.com:brenopacheco/.dotfiles.git',            '~/.dotfiles'         ],
@@ -35,29 +31,28 @@ clone(
     [ 'https://github.com/asdf-vm/asdf.git --branch v0.14.1', '~/.asdf'             ]
 );
 
-asdf(
-    nodejs => {
-        url => "https://github.com/twuni/asdf-yarn.git",
-        versions => [ "23.3.0", "22.11.0", "21.7.3", "20.18.1", "19.9.0", "18.20.5" ],
-        global => "23.3.0"
-    },
-    yarn => {
-        url => "https://github.com/twuni/asdf-yarn.git",
-        versions => [ "1.20.0" ],
-        global => "1.20.0"
-    }
-);
+asdf({
+    plugin => "nodejs",
+    url => "https://github.com/asdf-vm/asdf-nodejs.git",
+    versions => [ "18.20.5", "19.9.0", "20.18.1", "21.7.3", "22.11.0", "23.3.0" ],
+    global => "23.3.0"
+},{
+    plugin => "yarn",
+    url => "https://github.com/twuni/asdf-yarn.git",
+    versions => [ "1.20.0" ],
+    global => "1.20.0"
+});
 
 pacman(
     'arandr', 'base', 'base-devel', 'bash-completion', 'bat', 'bear',
     'blueman', 'bluez', 'bluez-utils', 'bottom', 'chromium', 'clang', 'cmake',
     'conky', 'cronie', 'ctags', 'dosfstools', 'dunst', 'dzen2', 'fd', 'feh',
     'file-roller', 'firefox', 'fzf', 'git', 'gnome-backgrounds', 'go',
-    'gparted', 'gpick', 'gtk2', 'gtk3', 'gtk4', 'gvim', 'helm', 'htop',
-    'imagemagick', 'inotify-tools', 'jq', 'k9s', 'lightdm',
-    'lightdm-gtk-greeter', 'lldb', 'lsof', 'lua', 'lua51', 'lua52', 'lua53',
-    'luajit', 'lua-language-server', 'luarocks', 'man-db', 'moreutils',
-    'ncdu', 'net-tools', 'network-manager-applet', 'ninja',
+    'gopls', 'gparted', 'gpick', 'gtk2', 'gtk3', 'gtk4', 'gvim', 'helm',
+    'htop', 'imagemagick', 'inotify-tools', 'jq', 'k9s', 'lightdm',
+    'lightdm-gtk-greeter', 'lf', 'lldb', 'lsof', 'lua', 'lua51', 'lua52',
+    'lua53', 'luajit', 'lua-language-server', 'luarocks', 'man-db',
+    'moreutils', 'ncdu', 'net-tools', 'network-manager-applet', 'ninja',
     'otf-comicshanns-nerd', 'pamixer', 'parted', 'pass', 'pass-otp',
     'pasystray', 'pavucontrol', 'pdftk', 'perl', 'perl-tidy', 'python',
     'python-pipx', 'redshift', 'renameutils', 'ripgrep', 'rsync', 'rust',
@@ -71,43 +66,43 @@ pacman(
 
 stow(cwd => '~/.dotfiles', target => '~/', package => 'home');
 
-dirs('~/git', '~/sketch', '~/tmp');
+dirs('~/git', '~/sketch', '~/tmp', '/tmp/xxx', '/tmp/yyy');
 
-etc(
-    { file => 'etc/lightdm-gtk-greeter.conf', dir => '/etc/lightdm/'         },
-    { file => 'etc/10-keyboard.rules',        dir => '/etc/udev/rules.d/'    },
-    { file => 'etc/40-wacom.rules',           dir => '/etc/udev/rules.d/'    },
-    { file => 'etc/10-keyboard.conf',         dir => '/etc/X11/xorg.conf.d/' },
-    { file => 'etc/20-monitor.conf',          dir => '/etc/X11/xorg.conf.d/' },
-    { file => 'etc/30-touchpad.conf',         dir => '/etc/X11/xorg.conf.d/' },
-);
+# etc(
+#     { file => 'etc/lightdm-gtk-greeter.conf', dir => '/etc/lightdm/'         },
+#     { file => 'etc/10-keyboard.rules',        dir => '/etc/udev/rules.d/'    },
+#     { file => 'etc/40-wacom.rules',           dir => '/etc/udev/rules.d/'    },
+#     { file => 'etc/10-keyboard.conf',         dir => '/etc/X11/xorg.conf.d/' },
+#     { file => 'etc/20-monitor.conf',          dir => '/etc/X11/xorg.conf.d/' },
+#     { file => 'etc/30-touchpad.conf',         dir => '/etc/X11/xorg.conf.d/' },
+# );
 
-systemctl(
-    root => [ 'bluetooth', 'cronie', 'docker', 'lightdm', 'sshd' ],
-    user => [ 'gpg-agent.target' ]
-);
+# systemctl(
+#     root => [ 'bluetooth', 'cronie', 'docker', 'lightdm', 'sshd' ],
+#     user => [ 'gpg-agent.target' ]
+# );
 
-makepkg(
-    'makepkg/fork' => [
-        'dmenu-fork',
-        'dwm-fork',
-        'slstatus-fork',
-        'st-fork',
-        'neovim'
-    ],
-    'makepkg/aur'  => [
-        'bun',
-        'helm-ls',
-        'lls-addons',
-        'marksman',
-        'obs-backgroundremoval',
-        'rose-pine-gtk-theme-full',
-        'slack-desktop',
-        'vscode-js-debug',
-        'xcursor-breeze',
-        'zoom'
-    ]
-);
+# makepkg(
+#     'makepkg/fork' => [
+#         'dmenu-fork',
+#         'dwm-fork',
+#         'slstatus-fork',
+#         'st-fork',
+#         'neovim'
+#     ],
+#     'makepkg/aur'  => [
+#         'bun',
+#         'helm-ls',
+#         'lls-addons',
+#         'marksman',
+#         'obs-backgroundremoval',
+#         'rose-pine-gtk-theme-full',
+#         'slack-desktop',
+#         'vscode-js-debug',
+#         'xcursor-breeze',
+#         'zoom'
+#     ]
+# );
 
 node(
     [ 'bash-language-server',              'bash-language-server'              ],
@@ -123,14 +118,11 @@ node(
 );
 
 go(
-    [ 'efmls',      'github.com/mattn/efm-langserver@latest'     ],
-    [ 'godoc',      'golang.org/x/tools/cmd/godoc@latest'        ],
-    [ 'gopls',      'golang.org/x/tools/gopls@latest'            ],
-    [ 'kind',       'sigs.k8s.io/kind@v0.20.0'                   ],
-    [ 'lazydocker', 'github.com/jesseduffield/lazydocker@latest' ],
-    [ 'lf',         'github.com/gokcehan/lf@latest'              ],
-    [ 'shfmt',      'mvdan.cc/sh/v3/cmd/shfmt@latest'            ],
-    [ 'yq',         'github.com/mikefarah/yq/v4@latest'          ]
+    [ 'efm-langserver', 'github.com/mattn/efm-langserver@latest'     ],
+    [ 'godoc',          'golang.org/x/tools/cmd/godoc@latest'        ],
+    [ 'kind',           'sigs.k8s.io/kind@v0.20.0'                   ],
+    [ 'lazydocker',     'github.com/jesseduffield/lazydocker@latest' ],
+    [ 'yq',             'github.com/mikefarah/yq/v4@latest'          ]
 );
 
 # }}} ------------------------------------------------------------------------
@@ -138,26 +130,128 @@ go(
 use diagnostics;
 use feature 'say';
 
-sub _system {
-    my ($shout, $cmd) = @_;
+my %cfg = ();
+
+sub _task {
+    my ($cmd) = @_;
     my @out = `$cmd 2>&1`;
-    if ($shout) {
-        print "  | $_" for (@out);
-    }
+    print "  | $_" for (@out);
     if ($? != 0) {
         print STDERR "\n[ERROR] command failed: $cmd\n";
         exit 1;
     }
-    return @out;
+}
+
+sub _check {
+    my ($cmd) = @_;
+    my $out = `$cmd 2>&1`;
+    return { ok => $? == 0, out => $out };
 }
 
 sub configure {
     my (%cfg) = @_;
     say "[OK] configuring";
-    # say "  * host: $cfg{host}";
-    # say "  * user: $cfg{user}";
-    # say "  * root: $cfg{root}";
+    while (my ($key, $val) = each %cfg) {
+        $cfg{$key} = $val;
+    }
 }
+
+sub clone {
+    my @in = @_;
+    my $dirs = join(" ", map { ${$_}[1] } @in);
+    my $out = _check("file -E $dirs")->{out};
+    my @res = $out =~ /^(.*):\s+ERROR: cannot stat/mg;
+    if (!@res) {
+        return say "[OK] clone"
+    }
+    say "[-] clone";
+    my $mlen = (sort { $b <=> $a } map { length(${$_}[0]) } @in)[0];
+    for my $dir (@res) {
+        my ($entry) = grep { $dir eq ${$_}[1] =~ s/~/\/home\/breno/r } @in;
+        my ($repo, $dir) = @{$entry};
+        printf "  * cloning: %-${mlen}s -> %s\n", $repo, $dir;
+        _task("git clone $repo $dir");
+    }
+}
+
+sub stow {
+    my (%cfg) = @_;
+    my $args = "--adopt -v 2 -d $cfg{cwd} -t $cfg{target} $cfg{package}";
+    my @links = grep { /^LINK/g } _check("stow -n $args");
+    if (!@links) {
+        return say "[OK] stow";
+    }
+    say "[-] stow";
+    say "  * stowing: $cfg{cwd}/$cfg{package} => $cfg{target}";
+    _task("stow $args 2>&1 | grep 'LINK'");
+}
+
+sub asdf {
+    my @tasks = ();
+    for my $conf (@_) {
+        my $check = _check("asdf list $conf->{plugin}");
+        my $installed = $check->{ok};
+        my %versions = map { $_ => 1 } $installed ? $check->{out} =~ /([^*\s]+)/mg : ();
+        my @missing = grep { ! exists $versions{$_} } @{$conf->{versions}};
+        my $current = $1 if $check->{out} =~ /\*(\S+)/;
+        if (!$installed) {
+            push @tasks, sub {
+                say "  * installing: $conf->{plugin} => $conf->{url}";
+                _task("asdf plugin add $conf->{plugin} $conf->{url}");
+            };
+        }
+        for my $version (@missing) {
+            push @tasks, sub {
+                say "  * installing: $conf->{plugin} => $version";
+                _task("asdf install $conf->{plugin} $version");
+            };
+        }
+        if (!$current || $current ne $conf->{global}) {
+            push @tasks, sub {
+                say "  * set-global: $conf->{plugin} => $conf->{global}";
+                _task("asdf global $conf->{plugin} $conf->{global}");
+            };
+        }
+    }
+    if (!@tasks) {
+        return say "[OK] asdf";
+    }
+    say "[-] asdf";
+    $_->() for @tasks;
+}
+
+sub pacman {
+    my $res = _check("pacman -Q @_");
+    if ($res->{ok}) {
+        return say "[OK] pacman";
+    }
+    my @packages = $res->{out} =~ /^error: package '(.*)' was not found/mg;
+    if (!@packages) {
+        return say "[OK] pacman";
+    }
+    say "[-] pacman";
+    say "  * installing: ", join(', ', @packages);
+    _task("sudo pacman -S --noconfirm --needed @packages");
+}
+
+sub dirs {
+    my $dirs = join(" ", @_);
+    my $out = _check("file -E $dirs")->{out};
+    my @res = $out =~ /^(.*):\s+ERROR: cannot stat/mg;
+    if (!@res) {
+        return say "[OK] dirs";
+    }
+    say "[-] dirs";
+    for my $dir (@res) {
+        say "  * mkdir: $dir";
+        _task("mkdir $dir");
+    }
+}
+
+sub systemctl {
+    say "[TODO] systemctl";
+}
+
 
 # TODO: what do we want to ensure?
 # 1. file exists (copy as is if not exist)
@@ -165,80 +259,6 @@ sub configure {
 # 3. respect original file ownership and permissions
 sub ensure {
     say "[TODO] ensure";
-}
-
-sub clone {
-    my @res = grep { `file -E ${$_}[1]\n` and $? != 0 } @_;
-    if (!@res) {
-        return say "[OK] clone"
-    }
-    say "[-] clone";
-    my $mlen = (sort { $b <=> $a } map { length(${$_}[0]) } @res)[0];
-    for (@res) {
-        my ($repo, $dir) = (@{$_});
-        printf "  * cloning: %-${mlen}s -> %s\n", $repo, $dir;
-        _system(1, "git clone $repo $dir");
-    }
-}
-
-sub stow {
-    my (%cfg) = @_;
-    my $args = "--adopt -v 2 -d $cfg{cwd} -t $cfg{target} $cfg{package}";
-    my @links = grep { /^LINK/g } _system(0, "stow -n $args 2>&1");
-    if (!@links) {
-        return say "[OK] stow";
-    }
-    say "[-] stow";
-    say "  * stowing: $cfg{cwd}/$cfg{package} => $cfg{target}";
-    _system(1, "stow $args 2>&1 | grep 'LINK'");
-}
-
-sub asdf {
-    # my %args = @_;
-    # my @installed_plugins = _system(0, `asdf plugin list`);
-    # while (my ($plugin, $value) = each %args) {
-    #     my @installed_versions = _system(0, `asdf list $plugin`);
-    #     my $url = $value->{url};
-    #     my @versions = @{$value->{versions}};
-    #     my $global = $value->{global};
-    #     say "Plugin: $plugin";
-    #     say "  > url:      $url";
-    #     say "  > versions: @versions";
-    #     say "  > global:   $global";
-    #     _system(1, "asdf plugin add $plugin $url");
-    #     for my $version (@versions) {
-    #         _system(1, "asdf install $plugin $version");
-    #     }
-    # }
-}
-
-sub pacman {
-    my $res = `pacman -Q @_ 2>&1`;
-    if ($? == 0) {
-        return say "[OK] pacman";
-    }
-    my @packages = $res =~ /error: package '(.*)' was not found/g;
-    say "[-] pacman";
-    for my $package (@packages) {
-        say "  * installing: $package" ;
-        _system(1, "sudo pacman -S --noconfirm --needed $package");
-    }
-}
-
-sub dirs {
-    my @dirs = grep { `file -E $_` and $? != 0 } @_;
-    if (!@dirs) {
-        return say "[OK] dirs";
-    }
-    say "[-] dirs";
-    for (@dirs) {
-        printf("  * mkdir: $_\n");
-        _system(1, "mkdir $_");
-    }
-}
-
-sub systemctl {
-    say "[TODO] systemctl";
 }
 
 # etc will scp all files in sync to a temporary dir, change permissions to
@@ -254,11 +274,43 @@ sub makepkg {
 }
 
 sub node {
-    say "[TODO] node";
+    my @pkgs = map { ${$_}[0] } @_;
+    my $check = _check("asdf list nodejs");
+    die "asdf nodejs plugin not found" if ! $check->{ok};
+    my @versions = $check->{out} =~ /([^*\s]+)/mg;
+    my $current = $1 if $check->{out} =~ /\*(\S+)/;
+    my @tasks = ();
+    for my $version (@versions) {
+        my $check = _check("ls ~/.asdf/installs/nodejs/$version/bin/");
+        my %bins = map { $_ => 1 } split(" ", $check->{out});
+        my @missing = grep { ! exists $bins{$_->[0]} } @_;
+        for my $ref (@missing) {
+            push @tasks, sub {
+                say "  * installing [$version]: $ref->[0]";
+                _task("source ~/.asdf/asdf.sh && asdf shell nodejs $version && npm install -g $ref->[1]")
+            }
+        }
+    }
+    if (!@tasks) {
+        return say "[OK] node";
+    }
+    say "[-] node";
+    $_->() for @tasks;
 }
 
 sub go {
-    say "[TODO] go";
+    my @pkgs = map { ${$_}[0] } @_;
+    my $check = _check("which @pkgs");
+    my %missing = map { $_ => 1 } $check->{out} =~ /^which: no (\S+) in/mg;
+    my @want = grep { exists $missing{$_->[0]} } @_;
+    if (!@want) {
+        return say "[OK] go";
+    }
+    say "[-] go";
+    for my $ref (@want) {
+        say "  * installing: $ref->[0]";
+        _task("go install $ref->[1]")
+    }
 }
 
 # }}} ------------------------------------------------------------------------
