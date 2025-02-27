862384a434c31bceb18ad0b31eda6e21039be0b8
Breno Leonhardt Pacheco <brenoleonhardt@gmail.com>
2025-02-27 15:58:02 +0000

    Thu 27 Feb 2025 03:58:03 PM WET

--- home/bin/udev/keyboard.sh
+++ home/bin/udev/keyboard.sh
@@ -4,6 +4,7 @@
 
 exec 1> >(logger -p user.notice -t keyboard)
 exec 2> >(logger -p user.err -t keyboard)
+export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
 
 echo "Starting configuration - $(whoami): ACTION $ACTION - HID_NAME: $HID_NAME"
 
@@ -12,10 +13,12 @@ ls /tmp/.X11-unix 2>/dev/null | sed 's/X//' | sort | uniq | while read -r DISPLA
 	echo "Configuring keyboard - DISPLAY $DISPLAY"
 	case "$HID_NAME" in
 	"Keyboard K380")
-		setxkbmap -model ppc105+inet -layout us
-		xrdb ~/.Xresources
-		xmodmap ~/.Xmodmap
-		xset r rate 200 30
+		(
+			sleep 1
+			~/bin/reset-keyboard
+			notify-send -i input-keyboard "$HID_NAME" "Keyboard reset"
+		) &
+		disown
 		;;
 	esac
 done
