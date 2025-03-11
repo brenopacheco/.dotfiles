7e038dafa0fd991643a85551b71503ff5504e614
Breno Leonhardt Pacheco <brenoleonhardt@gmail.com>
2025-03-11 09:29:51 +0000

    Tue 11 Mar 2025 09:29:51 AM WET

--- home/.config/emacs/init.el
+++
@@ -1,6 +0,0 @@
-(setq inhibit-startup-message t) ; no splash screen
-(scroll-bar-mode -1)             ; disable visibile scrollbar
-(tool-bar-mode -1)               ; disable toolbar
-(menu-bar-mode -1)               ; disable menu bar
-(set-fringe-mode 10)             ; horizontal padding
-(setq visible-bell f)            ; remove flashes
